7a7f0c27e9dcef00b5b4c8ee9089b88e425df643
sayda <sayda>
2017-02-06 10:04:43 -0500

    terminado

--- js/main.js
+++ js/main.js
@@ -1,60 +1,41 @@
-// desaparicion del nombre
-var span=document.createElement("span");
-function mensajeNombre(){
-    var nom=document.getElementById("name");
-    span.innerHTML="escriba correctamente su nombre";
-    nom.parentNode.appendChild(span);  
-}
-var spanNom=document.createElement("span");
-function nombreAviso(){
-    console.log("mamacita");
-    var nombre2=document.getElementById("name");
-    spanNom.innerHTML="debe ingresar su nombre";
-   nombre2.parentNode.appendChild(spanNom);
-}
-
-
-//desaparicion del apellido
-var apeSpan=document.createElement("span");
-function mensajeApellido(){
-    var apelli=document.getElementById("lastname");
-    apeSpan.innerHTML="escriba correctamente su apellido";
-    apelli.parentNode.appendChild(apeSpan);  
-}
-var apeSpan2=document.createElement("span");
-function mensajeAviso(){
-    var apellido2=document.getElementById("lastname");
-    apeSpan2.innerHTML="debe ingresar su apellido";
-    apellido2.parentNode.appendChild(apeSpan2);  
-}
-
-//desaparicion del correo
-var span3=document.createElement("span");
-function mensajeCorreo(){
-    var email = document.getElementById("input-email"); 
-    span3.innerHTML="verifique su correo";
-    email.parentNode.appendChild(span3);
-}
-
-//desaparicion de la contraseña
-var span4 =document.createElement("span");
-function  contrasenia (){
-    var password = document.getElementById("input-password");
-    span4.innerHTML="Su contraseña de tener almenos 6 caracteres";
-    password.parentNode.appendChild(span4);
-}
-
-//desaparcion del selec
-var span5=document.createElement("span");
-function mensajeSelec(){
-    var opcione= document.querySelector("select");
-    span5.innerHTML="elija una opcion";
-    opcione.parentNode.appendChild(span5);
-}
-
-//funcion general
-function validateForm()
+function removeMessage(_inputID)
 {
+	var elemento = document.getElementById(_inputID);
+
+	if(elemento.nextSibling != null)
+	{
+		elemento.parentNode.removeChild(elemento.nextSibling);
+	}
+}
+
+function crearSpan(_inputID, _message)
+{
+	var elemento = document.getElementById(_inputID);
+
+	if(elemento.nextSibling == null)
+	{
+		//El span no existe
+		var span = document.createElement('span');
+		span.innerHTML = _message;
+		elemento.parentNode.appendChild(span);
+	}else{
+		//El span ya existe
+		if(elemento.nextSibling.tagName == 'SPAN')
+		{
+			elemento.nextSibling.innerHTML = _message;
+		}else{
+			elemento.parentNode.removeChild(elemento.nextSibling);
+
+			var span = document.createElement('span');
+			span.innerHTML = _message;
+			elemento.parentNode.appendChild(span);
+		}
+	}
+}
+
+
+function validateForm(){
+	/* Escribe tú código aquí */
     var valiNombre = /^[a-zA-Z]*$/;
     var valiCorreo = /^([a-z]+[a-z1-9._-]*)@{1}([a-z1-9\.]{2,})\.([a-z]{2,3})$/;
     var nombre = document.getElementById("name").value;
@@ -63,74 +44,59 @@ function validateForm()
     var contra = document.getElementById("input-password").value;
     var twitter = document.getElementById("input-social").value;
     var selec= document.querySelector("select").value;
-    
-    if(nombre ==="" ||apellido===""||correo===""||contra===""){
-        nombreAviso();
-        mensajeAviso();
-        mensajeCorreo();
-        contrasenia ();
-        mensajeSelec();
-    }else{
-        spanNom.style.display="none";
-        apeSpan2.style.display="none";
-        span3.style.display="none";
-        span4.style.display="none";
-    }
-    
-    //validando Nombre
-    if (!valiNombre.test(nombre))
+    // validacion del nombre
+    if(nombre===""){
+        crearSpan("name" , "Ingrese su nombre.")
+    }else if (!valiNombre.test(nombre))
     {
-        mensajeNombre();
+        crearSpan("name","Escriba su nombre correctamente")
     }else{
         if(nombre.toLowerCase())
         {   
             document.getElementById("name").value=nombre.charAt(0).toUpperCase()+nombre.slice(1);
-            span.style.display="none";
-            mensajeNombre();
+            removeMessage("name");
         }else{
-            nombreAviso(); 
-            span.style.display="none";
+           crearSpan("name" , "Escriba su nombre correctamente");
+            removeMessage("name");
         }  
     }
  
-  //validando Apellido
-    if (!valiNombre.test(apellido))
+    
+    //validacion del apellido
+    if (apellido===""){
+        crearSpan("lastname" , "Ingrese su apellido");
+    }else if (!valiNombre.test(apellido))
     {
-        mensajeApellido();
+        crearSpan("lastname","Escriba su apellido correctamente")
     }else{
         if(apellido.toLowerCase())
-        {
+        {   
             document.getElementById("lastname").value=apellido.charAt(0).toUpperCase()+apellido.slice(1);
-            mensajeApellido();
-            apeSpan.style.display="none";
+            removeMessage("lastname");
         }else{
-            mensajeAviso(); 
+           crearSpan("lastname" , "Escriba su apellido correctamente");
+            removeMessage("lastname");
         }  
     }
     
-  //validando Correo
- if (!valiCorreo.test(correo)){
-        mensajeCorreo();
-        return false; 
+    //validacion del correo
+    if (!valiCorreo.test(correo)){
+        crearSpan("input-email" , "Verefique su correo") ;
     }else{
-       span3.style.display="none";
+       removeMessage("input-email");
     }
- 
- //validando la contraseña
- if (contra.length<=6){
-        contrasenia();
+    //validacion de la contraseña
+    if (contra.length<=6){
+        crearSpan("input-password" , "su contraseña debe tener al menos 6 caracteres");
        
     }else{
-       
-       span4.style.display="none";
+       removeMessage("input-password");
     } 
-   
-    
-    // validando el selec opciones
-    if (selec ==0) {
-       mensajeSelec();
-        return false;
+    // validacion de los elecion  tipo de bicicleta
+    var selec= document.getElementById("elejir").value;
+    if (selec==0) {
+       crearSpan("elejir" , "Debe elejir una opcion.")
     } else {
-        span5.style.display="none";
+       removeMessage("elejir");
     } 
 }
