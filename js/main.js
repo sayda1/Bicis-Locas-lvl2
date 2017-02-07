21bc34efebed242b4edb8f5d23f8cb0ff6319c8b
sayda <sayda>
2017-02-07 08:37:56 -0500

    corregido contraseña

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 function removeMessage(_inputID)
 {
 	var elemento = document.getElementById(_inputID);
-
 	if(elemento.nextSibling != null)
 	{
 		elemento.parentNode.removeChild(elemento.nextSibling);
@@ -11,31 +10,25 @@ function removeMessage(_inputID)
 function crearSpan(_inputID, _message)
 {
 	var elemento = document.getElementById(_inputID);
-
 	if(elemento.nextSibling == null)
 	{
-		//El span no existe
-		var span = document.createElement('span');
+		var span = document.createElement("span");
 		span.innerHTML = _message;
 		elemento.parentNode.appendChild(span);
 	}else{
-		//El span ya existe
-		if(elemento.nextSibling.tagName == 'SPAN')
+		if(elemento.nextSibling.tagName == "SPAN")
 		{
 			elemento.nextSibling.innerHTML = _message;
 		}else{
 			elemento.parentNode.removeChild(elemento.nextSibling);
-
-			var span = document.createElement('span');
+			var span = document.createElement("span");
 			span.innerHTML = _message;
 			elemento.parentNode.appendChild(span);
 		}
 	}
 }
 
-
 function validateForm(){
-	/* Escribe tú código aquí */
     var valiNombre = /^[a-zA-Z]*$/;
     var valiCorreo = /^([a-z]+[a-z1-9._-]*)@{1}([a-z1-9\.]{2,})\.([a-z]{2,3})$/;
     var nombre = document.getElementById("name").value;
@@ -49,45 +42,43 @@ function validateForm(){
         crearSpan("name" , "Ingrese su nombre.")
     }else if (!valiNombre.test(nombre))
     {
-        crearSpan("name","Escriba su nombre correctamente")
+        crearSpan("name","Escriba su nombre correctamente.")
     }else{
         if(nombre.toLowerCase())
         {   
             document.getElementById("name").value=nombre.charAt(0).toUpperCase()+nombre.slice(1);
             removeMessage("name");
         }else{
-           crearSpan("name" , "Escriba su nombre correctamente");
+           crearSpan("name" , "Escriba su nombre correctamente.");
             removeMessage("name");
         }  
     }
- 
-    
     //validacion del apellido
     if (apellido===""){
-        crearSpan("lastname" , "Ingrese su apellido");
+        crearSpan("lastname" , "Ingrese su apellido.");
     }else if (!valiNombre.test(apellido))
     {
-        crearSpan("lastname","Escriba su apellido correctamente")
+        crearSpan("lastname","Escriba su apellido correctamente.")
     }else{
         if(apellido.toLowerCase())
-        {   
-            document.getElementById("lastname").value=apellido.charAt(0).toUpperCase()+apellido.slice(1);
+        {    document.getElementById("lastname").value=apellido.charAt(0).toUpperCase()+apellido.slice(1);
             removeMessage("lastname");
         }else{
-           crearSpan("lastname" , "Escriba su apellido correctamente");
+           crearSpan("lastname" , "Escriba su apellido correctamente.");
             removeMessage("lastname");
         }  
     }
-    
     //validacion del correo
     if (!valiCorreo.test(correo)){
-        crearSpan("input-email" , "Verefique su correo") ;
+        crearSpan("input-email" , "Verefique su correo.") ;
     }else{
        removeMessage("input-email");
     }
     //validacion de la contraseña
-    if (contra.length<=6){
-        crearSpan("input-password" , "su contraseña debe tener al menos 6 caracteres");
+    if (contra==="password" || contra==="123456"||contra==="098754"){
+        crearSpan("input-password" , "Su contraseña no es valido.");
+    }else if (contra.length<=6){
+        crearSpan("input-password" , "Su contraseña debe tener al menos 6 caracteres.");
        
     }else{
        removeMessage("input-password");
